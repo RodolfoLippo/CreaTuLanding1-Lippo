cb1baf2438f1a3145952465182e62ac931b52bcf
RodolfoLippo <rodolfolippo0405@gmail.com>
2025-05-15 22:43:20 +0200

    Proyecto Final Rodolfo Lippo

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { CartProvider } from './context/cart_context.jsx';
-import './index.css'; // si tienes estilos globales
+import { CartProvider } from './context/CartContext.jsx';
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
